b7b1b973fdfaefab322359c906d68e6e61e40a9d
Kaylan Barriault <kay31286@gmail.com>
2026-03-14 19:37:17 -0300

    Add basic C++ program to output text and input
    Implement a simple C++ program that outputs text and takes user input for a tip amount.

---
+++ hi.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(){
+std::cout <<"Hello World!\n";
+
+std::cout <<"goodbye\tbruh\n";
+
+std::cout <<" 1\n";
+std::cout <<" 2 3\n";
+std::cout <<" 4 5 6\n";
+std::cout <<"7 8 9 10\n";
+//aye im making a comment
+
+int tip = 0;
+
+std::cout << "Enter tip amount: ";
+std::cin >> tip;
+std::cout << tip << "\n";
+}
+
+/* long one
+
+like this
+*/
